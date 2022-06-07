b30ae722350a56b82d2cff1b13e55140654add31
Tsubasa Endo <tsubasa.bneaus@gmail.com>
2022-06-07 15:13:37 +1000

    I integrated Dinesh's database codes to the current program

---
+++ test/guiExploration/MainMenuTest.java
@@ -0,0 +1,16 @@
+package guiExploration;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainMenuTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void actionPerformed() {
+    }
+}
---
+++ test/guiExploration/SignUpTest.java
@@ -0,0 +1,12 @@
+package guiExploration;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SignUpTest {
+
+    @Test
+    void actionPerformed() {
+    }
+}
